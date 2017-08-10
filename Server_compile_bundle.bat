4042404f14495fc8aa582914a833facdc139bd71
koloa <koltsov.oa@yandex.ru>
2017-08-10 12:33:42 +0300

    update treatments report

--- Server_compile_bundle.bat
+++ Server_compile_bundle.bat
@@ -1,2 +1,3 @@
 node app.js
 copy f:\Projects\NightScout\cgm-remote-monitor\bundle\bundle.out.js f:\Projects\NightScout\cgm-remote-monitor\static\public\js\bundle.js /y
+copy f:\Projects\NightScout\cgm-remote-monitor\bundle\bundle.out.js f:\Projects\NightScout\cgm-remote-monitor\bundle\bundle.js /y
