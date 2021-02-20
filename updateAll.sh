c5b92603444baceb1ce5f9b2407b9b69d60524db
14687875 <koltsov.o.al@sberbank.ru>
2021-02-20 17:47:27 +0300

    Update to 14.2.1
    Merge branch 'master' of https://github.com/nightscout/cgm-remote-monitor into master
    
    # Conflicts:
    #	bower.json
    #	lib/client/boluscalc.js
    #	lib/client/chart.js
    #	lib/client/index.js
    #	lib/client/renderer.js
    #	lib/food/food.js
    #	lib/language.js
    #	lib/profile/profileeditor.js
    #	lib/report_plugins/daytoday.js
    #	lib/report_plugins/treatments.js
    #	lib/server/treatments.js
    #	package-lock.json
    #	package.json
    #	static/css/drawer.css
    #	static/css/report.css
    #	static/index.html
    #	static/js/client.js
    #	static/report/index.html
    #	static/report/js/compare.js
    #	static/report/js/report.js
    #	views/foodindex.html

--- updateAll.sh
+++ updateAll.sh
@@ -3,3 +3,4 @@ rm -R cgm-remote-monitor
 git clone https://github.com/padshiy8angel/cgm-remote-monitor
 cd cgm-remote-monitor
 npm install &&   npm run postinstall &&   npm run env &&   npm audit fix
+sh start.sh
